c059409dba4802c390040b7a147469eee2bba121
Svetlana Fedorenko <svetlana493@ukr.net>
2022-08-28 09:17:45 +0300

    дз8

---
+++ src/components/AuthMenu/AuthMenu.jsx
@@ -0,0 +1,29 @@
+import { Link } from 'react-router-dom';
+import { Button } from '@mui/material';
+
+export default function AuthMenu({ setAnchorElNav }) {
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+  return (
+    <>
+      <Link to={'/register'} style={{ textDecoration: 'none', color: 'white' }}>
+        <Button
+          onClick={handleCloseNavMenu}
+          sx={{ my: 2, color: 'white', display: 'block' }}
+        >
+          Register
+        </Button>
+      </Link>
+
+      <Link to={'/login'} style={{ textDecoration: 'none', color: 'white' }}>
+        <Button
+          onClick={handleCloseNavMenu}
+          sx={{ my: 2, color: 'white', display: 'block' }}
+        >
+          Login
+        </Button>
+      </Link>
+    </>
+  );
+}
